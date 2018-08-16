2c5870c2fd3ac9bd5ca1b745e6958da2ea8ba6ed
kenghuo <354396624@qq.com>
2018-08-16 17:04:51 +0800

    webpack primer

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Uglify = require('uglifyjs-webpack-plugin');
+
 module.exports = {
-    entry: __dirname + '/app/main.js', //已多次提及的唯一入口文件
+    entry: __dirname + '/app/main.js', //入口文件
     output: {
-        path: __dirname + '/', //打包后的文件存放的地方
+        path: __dirname + '/dist', //打包后的文件存放的目录
         filename: 'bundle.js' //打包后输出文件的文件名
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                loader: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            minimize: true,
+                            sourceMap: true,
+                            importLoader: 2
+                        }
+                    },{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true,
+                            sourceMap: true,
+                            importLoader: 2
+                        }
+                    }
+                ]
+                
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // html引用自动插入
+            filename: 'index.html',
+            template: 'app/index.html'
+        }),
+        new webpack.BannerPlugin(`@author: 'kenghuo'\n@name: 'webpack 入门读物'`), // 版权
+        new MiniCssExtractPlugin({      // css单独分离
+            filename: "[name].css",
+        }),
+        new Uglify()
+    ]
 }
