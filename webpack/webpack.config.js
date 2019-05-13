213cc04ddfcaeb836ec6ace20fcb9c634230b1dc
kenghuo <354396624@qq.com>
2019-05-13 17:22:56 +0800

    build js bug

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
                     }
                 ]
                 
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: 'babel-loader',
+                options:{
+                    "presets": ["@babel/preset-env"]
+                }
             }
         ]
     },
